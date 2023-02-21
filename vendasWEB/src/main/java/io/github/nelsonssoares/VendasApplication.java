4ee175bce7c9e16902c9b2b9e1a95fbfe36f61c0
Nelson <punkpunkada@gmail.com>
2023-02-21 10:55:04 -0300

    Controller e annotations

--- vendasWEB/src/main/java/io/github/nelsonssoares/VendasApplication.java
+++ vendasWEB/src/main/java/io/github/nelsonssoares/VendasApplication.java
@@ -1,49 +1,11 @@
 package io.github.nelsonssoares;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.github.nelsonssoares.domain.entity.Cliente;
-import io.github.nelsonssoares.domain.entity.Pedido;
-import io.github.nelsonssoares.domain.repository.Clientes;
-import io.github.nelsonssoares.domain.repository.Pedidos;
 
 @SpringBootApplication
 public class VendasApplication {
 	
-	@Bean
-	public CommandLineRunner init(@Autowired Clientes clientes,  @Autowired Pedidos pedidos) {
-		return args -> {
-			
-			System.out.println("Salvando cliente");
-			Cliente cliente = new Cliente();
-			
-			cliente.setNome("Nelson");
-			clientes.save(cliente);	
-			
-			Pedido p = new Pedido();
-			p.setCliente(cliente);
-			p.setDataPedido(LocalDate.now());
-			p.setTotal(BigDecimal.valueOf(100));
-			pedidos.save(p);
-//			
-//			Cliente nelson = clientes.findClienteFetchPedidos(cliente.getId());
-//			
-//			System.out.println(nelson);
-//			System.out.println(nelson.getPedidos());
-			
-			pedidos.findByCliente(cliente).forEach(System.out::println);
-			
-			
-			
-			
-		};
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
---
+++ vendasWEB/src/main/java/io/github/nelsonssoares/rest/controller/ClienteController.java
@@ -0,0 +1,23 @@
+package io.github.nelsonssoares.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/clientes")
+public class ClienteController {
+	
+	@RequestMapping(value = "/hello/{nome}",
+					method = RequestMethod.GET,
+					consumes = {"application/json", "application/xml"},
+					produces = {"application/json", "application/xml"}
+					)
+	@ResponseBody
+	public String helloClientes(@PathVariable("nome") String nomeCliente) {
+		return String.format("Hello %s", nomeCliente);
+	}
+	
+}
