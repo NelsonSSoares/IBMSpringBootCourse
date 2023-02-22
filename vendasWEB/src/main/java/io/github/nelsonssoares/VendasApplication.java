56c36d946622c9289e38e01bf12072524d444d77
Nelson <punkpunkada@gmail.com>
2023-02-22 11:41:59 -0300

    Get Method and ResponseEntity

--- vendasWEB/src/main/java/io/github/nelsonssoares/VendasApplication.java
+++ vendasWEB/src/main/java/io/github/nelsonssoares/VendasApplication.java
@@ -1,12 +1,26 @@
 package io.github.nelsonssoares;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.nelsonssoares.domain.entity.Cliente;
+import io.github.nelsonssoares.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
 	
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
+		return args -> {
+			Cliente c = new Cliente(null, "Nelson");
+			clientes.save(c); 
+		};
+	}
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
--- vendasWEB/src/main/java/io/github/nelsonssoares/rest/controller/ClienteController.java
+++ vendasWEB/src/main/java/io/github/nelsonssoares/rest/controller/ClienteController.java
@@ -1,23 +1,56 @@
 package io.github.nelsonssoares.rest.controller;
 
+import java.net.http.HttpHeaders;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.github.nelsonssoares.domain.entity.Cliente;
+import io.github.nelsonssoares.domain.repository.Clientes;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 	
-	@RequestMapping(value = "/hello/{nome}",
-					method = RequestMethod.GET,
-					consumes = {"application/json", "application/xml"},
-					produces = {"application/json", "application/xml"}
-					)
+//	@RequestMapping(value = "/hello/{nome}",
+//					method = RequestMethod.GET,
+//					consumes = {"application/json", "application/xml"},
+//					produces = {"application/json", "application/xml"}
+//					)
+//	@ResponseBody
+//	public String helloClientes(@PathVariable("nome") String nomeCliente) {
+//		return String.format("Hello %s", nomeCliente);
+//	}
+	
+	private Clientes clientes;
+	
+	public ClienteController(Clientes clientes) {
+		this.clientes = clientes;
+	}
+	
+	@GetMapping("/{id}")
 	@ResponseBody
-	public String helloClientes(@PathVariable("nome") String nomeCliente) {
-		return String.format("Hello %s", nomeCliente);
+	public ResponseEntity<Cliente> getClienteById (@PathVariable Integer id) {
+		
+		Optional<Cliente> cliente = clientes.findById(id);
+		
+		if(cliente.isPresent()) {		
+//			HttpHeaders headers = new HttpHeaders();
+//			headers.put("Authorization", "token")		
+//			ResponseEntity<Cliente> responseEntity = new ResponseEntity<Cliente>(cliente.get() , headers , HttpStatus.OK, );
+			
+			return ResponseEntity.ok(cliente.get()); //retorna cliente caso ecista com codigo status 200
+		}
+		
+		return ResponseEntity.notFound().build(); // cliente não encontrado 404
+		
+				
 	}
 	
 }
