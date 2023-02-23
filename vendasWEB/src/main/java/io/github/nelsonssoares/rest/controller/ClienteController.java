705f431fb2c855792fc80c0c8dea5aa8f545c849
Nelson <punkpunkada@gmail.com>
2023-02-23 16:50:03 -0300

    Put Method

--- vendasWEB/src/main/java/io/github/nelsonssoares/rest/controller/ClienteController.java
+++ vendasWEB/src/main/java/io/github/nelsonssoares/rest/controller/ClienteController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -75,6 +76,17 @@ public class ClienteController {
 			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseBody
+	public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente) {
+		
+		return clientes.findById(id).map(clienteExistente ->{
+			cliente.setId(clienteExistente.getId());
+			clientes.save(cliente);
+			return ResponseEntity.noContent().build();
+		}).orElseGet( () -> ResponseEntity.notFound().build() );
 	}
 	
 }
